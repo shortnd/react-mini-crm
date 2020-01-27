d5dbfdaee1c78ee544992f520a331e944d5ceabe
Collin OConnell <ococncol@gmail.com>
2020-01-27 16:01:01 -0500

    starting demo app with miragejs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+export default function App() {
+  let [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/users")
+      .then(res => res.json())
+      .then(json => {
+        setUsers(json.users)
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ul data-testid="users">
+      {users.map(user => (
+        <li key={user.id} data-testid={`user-${user.id}`}>
+          {user.name}
+        </li>
+      ))}
+    </ul>
+  )
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { makeServer } from "./server"
+
+if (process.env.NODE_ENV === "development") {
+  makeServer();
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
